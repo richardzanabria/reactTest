414d059555d3ed656bae93dc4f693b18f8a87c40
richardzanbria <richardzanabria612@gmail.com>
2024-02-19 21:09:10 +0300

    fix return button

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -174,12 +174,11 @@ const Checkout = () => {
     </div>
     <div className="btn-row w-100 d-flex justify-content-between">
       <div className="back-link">
-        <Link to="/">
-          <span>Return to shipping</span>
-          {/* <svg focusable="false" aria-hidden="true" className="icon-svg icon-svg--color-accent icon-svg--size-10 previous-link__icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10">
+        <Link to="/" className="flex">
+          <svg focusable="false" aria-hidden="true" className="icon-svg icon-svg--color-accent icon-svg--size-10 previous-link__icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10">
             <path d="M8 1L7 0 3 4 2 5l1 1 4 4 1-1-4-4"></path>
-          </svg> */}
-
+          </svg>
+          <span>Return to shipping</span>
         </Link>
       </div>
       <button className="checkout-btn">Checkout now</button>
